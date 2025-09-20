8a9a5968c9a0f9b7d49f1753f0a0b5665fbb13ee
Vinita Meena <vinitameena776@gmail.com>
2025-09-20 21:25:02 +0530

    Third code

---
+++ 4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter value of a:\n";
+    cin>>a;
+    cout<<"enter value of b:\n";
+    cin>>b;
+    
+    if(a>b){
+        cout<<"a is greater";
+    }
+    if(a<b){
+        cout<<"b is greater";
+    }
+    return 0;
+}
